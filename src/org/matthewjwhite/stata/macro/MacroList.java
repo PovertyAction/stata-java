3af915ac07bd4767e24c77e5030085c353f58c75
matthew-white <mwhite@poverty-action.org>
2015-02-11 14:22:34 -0500

    MacroList.invtokens() takes Object[]

--- src/org/matthewjwhite/stata/macro/MacroList.java
+++ src/org/matthewjwhite/stata/macro/MacroList.java
@@ -3,18 +3,26 @@ package org.matthewjwhite.stata.macro;
 import org.matthewjwhite.stata.string.Quotes;
 
 public class MacroList {
-	public static String invtokens(String[] list, Quotes.Style style) {
+	public static String invtokens(Object[] list, Quotes.Style style) {
 		StringBuilder macroList = new StringBuilder();
 		for (int i = 0; i < list.length; i++) {
 			if (i > 0)
 				macroList.append(" ");
-			macroList.append(Quotes.adorn(list[i], style));
+			macroList.append(Quotes.adorn(list[i].toString(), style));
 		}
 
 		return macroList.toString();
 	}
 
-	public static String invtokens(String[] list) {
+	public static String invtokens(Object[] list) {
 		return invtokens(list, Quotes.Style.LIST);
+	}
+
+	public static String invtokens(String[] list, Quotes.Style style) {
+		return invtokens((Object[]) list, style);
+	}
+
+	public static String invtokens(String[] list) {
+		return invtokens((Object[]) list);
 	}
 }
